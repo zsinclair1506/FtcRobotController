e314f576ab66f390425a9feb194c29893e8c76ed
devon <froggerman330@gmail.com>
2020-10-06 20:19:19 +1100

    Messy Singleton implementation example and javadoc.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/XDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/XDrive.java
@@ -8,8 +8,26 @@ import org.firstinspires.ftc.teamcode.lib.RotationDirection;
 
 public class XDrive extends DriveBase {
 
+    private static XDrive instance = null;
 
-    public XDrive(HardwareMap map) {
+    /***
+     * Messy singleton implementation.
+     * @param map the hardware map of the robot/phone/expansion hub.
+     * @return the existing instance of the X DriveBase or a new instance on first time.
+     */
+    public static XDrive getInstance(HardwareMap map) {
+        if(instance == null) {
+            instance = new XDrive(map);
+        }
+
+        return instance;
+    }
+
+    /***
+     * Private constructor that creates the motor map for each motor on the robot.
+     * @param map the hardware map of the robot/phone/expansion hub.
+     */
+    private XDrive(HardwareMap map) {
 
         this.motors.put("frontLeft", map.get(DcMotor.class, "frontLeft"));
         this.motors.put("frontRight", map.get(DcMotor.class, "frontRight"));
@@ -17,21 +35,41 @@ public class XDrive extends DriveBase {
         this.motors.put("backLeft", map.get(DcMotor.class, "backLeft"));
     }
 
+    /***
+     *
+     * @param angle
+     * @param power
+     */
     @Override
     public void DrivePower(float angle, float power) {
 
     }
 
+    /***
+     *
+     * @param angle
+     * @param distance
+     */
     @Override
     public void DriveDistance(float angle, float distance) {
 
     }
 
+    /***
+     *
+     * @param angle
+     * @param power
+     */
     @Override
     public void RotateAngle(float angle, float power) {
 
     }
 
+    /***
+     *
+     * @param direction
+     * @param power
+     */
     @Override
     public void RotateDirection(RotationDirection direction, float power) {
 
