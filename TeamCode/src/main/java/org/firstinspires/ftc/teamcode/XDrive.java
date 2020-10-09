ae0f4a0ca5e64d6dd4791f2395094300634cdd18
devon <froggerman330@gmail.com>
2020-10-09 19:53:30 +1100

    Remove singleton. We will have to practice good hygiene and not create multiple drivebases.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/XDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/XDrive.java
@@ -8,26 +8,13 @@ import org.firstinspires.ftc.teamcode.lib.RotationDirection;
 
 public class XDrive extends DriveBase {
 
-    private static XDrive instance = null;
 
     /***
-     * Messy singleton implementation.
+     *
      * @param map the hardware map of the robot/phone/expansion hub.
      * @return the existing instance of the X DriveBase or a new instance on first time.
      */
-    public static XDrive getInstance(HardwareMap map) {
-        if(instance == null) {
-            instance = new XDrive(map);
-        }
-
-        return instance;
-    }
-
-    /***
-     * Private constructor that creates the motor map for each motor on the robot.
-     * @param map the hardware map of the robot/phone/expansion hub.
-     */
-    private XDrive(HardwareMap map) {
+    public void XDrive (HardwareMap map) {
 
         this.motors.put("frontLeft", map.get(DcMotor.class, "frontLeft"));
         this.motors.put("frontRight", map.get(DcMotor.class, "frontRight"));
