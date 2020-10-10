13f67a395db4749a85724de44fec3ac372d86b6b
BLHodder <61534159+BLHodder@users.noreply.github.com>
2020-10-10 16:16:19 +1100

    Created Switcher class
    The class for the switcher mechanism on the robot was created

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Switcher.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import org.firstinspires.ftc.teamcode.lib.RotationDirection;
+
+/***
+ * this class is for the switcher mechanism on the robot
+ */
+public class Switcher extends Mechanism {
+    //Private ?? motors
+
+    /***
+     * pushes a ring in a certain direction
+     * @param direction to move the ring
+     */
+    public void MoveRing(RotationDirection direction){
+
+    }
+}
