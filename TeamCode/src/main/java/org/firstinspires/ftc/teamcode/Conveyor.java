0eeeffb3de3623092005692548cf4769e3b8e82c
zsinclair1506 <60305207+zsinclair1506@users.noreply.github.com>
2020-10-10 15:56:43 +1100

    Created Conveyor class
    Created the class responsible for running the conveyor mechanism

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Conveyor.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+/***
+ * The class responsible for the running of the conveyor mechanism on the robot
+ */
+public class Conveyor extends Mechanism {
+    //private ?? motor
+    //private ?? ringpresent
+
+    /***
+     * Convey rings from the intake to the shooting mechanism, with a @power [0-1]
+     * @param power the power with which to push the rings through the conveyor
+     */
+    public void Convey(double power){
+
+    }
+
+    /***
+     * Conveys rings from the intake to the shooting mechanism with a preset power
+     */
+    public void Convey(){
+
+    }
+
+}
