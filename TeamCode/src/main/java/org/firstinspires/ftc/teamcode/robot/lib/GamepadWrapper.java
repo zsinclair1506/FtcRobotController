113cb133d83a2c4d6d8ca90513274a5290bedf6c
Devon <froggerman330+git@gmail.com>
2020-10-12 21:16:04 +1100

    Create wrapper for gamepad to allow field access with getter
    wraps a gamepad object to allow gamepadwarpper.get(button/joystick) to get value.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/lib/GamepadWrapper.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode.robot.lib;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import java.lang.reflect.Field;
+
+/***
+ * Class to wrap a gamepad object to allow Object Oriented field access.
+ * This allows mapping between ENUM values and gamepad fields.
+ */
+public class GamepadWrapper {
+    private Gamepad gamepad;
+    private Class gamepadClass = Gamepad.class; // used for reflection to get the fields
+
+    /***
+     * Constructor for the @GamepadWrapper
+     * @param gamepad the gamepad that this wraps (around)
+     */
+    public GamepadWrapper(Gamepad gamepad){
+        this.gamepad = gamepad;
+    }
+
+    /***
+     * Gets the value of the button @buttonName from the gamepad
+     * @param buttonName the button to get the value of
+     * @return true if the button is pressed
+     */
+    public boolean getButton(String buttonName) {//"a"
+        try {
+            Field button = gamepadClass.getField(buttonName);
+            return (Boolean) button.get(gamepad);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            //ignore exceptions, return false
+            return false;
+        }
+    }
+
+    /***
+     * Gets the value of the joystic @joystickAxis from the gamepad
+     * @param joystickAxis the joystick axis to get the value of
+     * @return float value of the joystick axis
+     */
+    public float getJoystick(String joystickAxis){
+        try {
+            Field joystick = gamepadClass.getField(joystickAxis);
+            return (float) joystick.get(gamepad);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            //ignore exceptions, return false
+            return 0.0f;
+        }
+    }
+
+    /***
+     * Gets the value of the trigger @trigger from the gamepad
+     * @param trigger the trigger to get the value of
+     * @return float value of the trigger
+     */
+    public float getTrigger(String trigger){
+        return getJoystick(trigger);
+    }
+}
