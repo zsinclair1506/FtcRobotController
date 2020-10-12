8656ff2088754436bfc9de06dda166bcff375918
Devon <froggerman330+git@gmail.com>
2020-10-12 19:31:47 +1100

    Created ramp modes as enum
    Ramp modes for how to ramp changes between two values

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/lib/RampMode.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.robot.lib;
+
+/***
+ * RampModes are how a change in value can be controlled.
+ * SCURVE - low acceleration in and out of the ramp up
+ * TRAPAZOID - straight line between two points
+ */
+public enum RampMode {
+    SCURVE, TRAPEZOID
+}
