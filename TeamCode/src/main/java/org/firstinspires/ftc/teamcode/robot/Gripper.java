683e3b9ddfd42b4ca11754bf4977620140507e03
BLHodder <61534159+BLHodder@users.noreply.github.com>
2020-10-21 21:14:41 +1100

    Gripper Class was created
    Everything in the gripper class has been set up and is ready for further work

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Gripper.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.robot;
+
+import org.firstinspires.ftc.teamcode.robot.lib.GripperPosition;
+
+/***
+ * The class is controlling the Gripper mechanism
+ */
+public class Gripper extends Mechanism {
+
+    /***
+     *  Closes the Gripper mechanism
+     */
+    public void closeGripper() {
+
+    }
+
+    /***
+     * OPens the gripper mechanism
+     */
+    public void openGripper(){
+
+    }
+
+    /***
+     *   Gets if the Gripper is open or not
+     * @return true if Gripper is open
+     */
+    public boolean getIsOPen(){
+        return true;
+    }
+
+    /***
+     *  tilts the Gripper mechanism up
+     */
+    public void tiltUp(){
+
+   }
+
+    /***
+     *  tilts the Gripper mechansim down
+     */
+   public void tiltDown(){
+
+   }
+
+    /***
+     *  Setting the position of the gripper from Ring or Wobble Goal
+     * @param position to move the arm to
+     */
+    public void moveToPos(GripperPosition position){
+
+    }
+
+}
