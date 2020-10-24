cddfabba4525000b6b1f47e4fa50dc6d6dd2f298
Devon <froggerman330+git@gmail.com>
2020-10-24 12:25:52 +1100

    removed unused import

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Loader.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Loader.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.robot;
 
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
