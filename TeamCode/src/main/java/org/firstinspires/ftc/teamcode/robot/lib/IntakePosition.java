df3e8f816e61795515fe162bd050fe7204a66d94
Devon <froggerman330+git@gmail.com>
2020-10-25 14:13:58 +1100

    updated the intake based on the new mechanical design

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/lib/IntakePosition.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.robot.lib;
+
+public enum IntakePosition {
+    DROP_OFF, INTAKE
+
+    //will likely add pre-programmed servo positions for this
+    /*
+    TODO make constructor
+    TODO private double position
+     */
+}
