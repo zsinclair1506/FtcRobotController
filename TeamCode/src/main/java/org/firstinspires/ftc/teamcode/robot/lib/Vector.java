3fb6ba8f6928c0a0a164990e60960ece38dc404b
Devon <froggerman330+git@gmail.com>
2020-10-29 11:34:35 +1100

    vector class for values with magnitude and direction :)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/lib/Vector.java
@@ -0,0 +1,187 @@
+package org.firstinspires.ftc.teamcode.robot.lib;
+
+/***
+ * Vector class defining things for 2 and 3 dimensional vectors. While some things will work with
+ * n-dimensional vectors, cross products in n-dimensional space are too complicated for this simple
+ * mentor and so we are going to artificially limit that method.
+ */
+public class Vector {
+    private double values[];
+
+    public static Vector X_2 = new Vector(2,1, 0);
+    public static Vector X_3 = new Vector(3, 1, 0, 0);
+    public static Vector Y_2 = new Vector(2,0, 1);
+    public static Vector Y_3 = new Vector(3, 0, 1, 0);
+    public static Vector Z_3 = new Vector(3, 0, 0, 1);
+
+
+    /***
+     * Create a vector with
+     * @param inVaules
+     */
+    public Vector(double... inVaules) {
+        this.values = inVaules;
+    }
+
+    /***
+     * Create a vector from magnitude and angle, only works for 2D vectors.
+     * @param magnitude the magnitude of the vector (length)
+     * @param angle the angle of the vector from the X axis
+     */
+    public Vector(double magnitude, double angle){
+        this.values = new double[]{magnitude * Math.cos(angle), magnitude * Math.sin(angle)};
+    }
+
+    /***
+     * Create a vector with all 1s in the values.
+     * @param dimensions the number of axes for the vector
+     */
+    public Vector(int dimensions){
+        this.values = new double[dimensions];
+        for(int i = 0; i < dimensions; i++){
+            this.values[i] = 1;
+        }
+    }
+
+    /***
+     *
+     * @return
+     */
+    public Vector normalise() {
+        double d = this.getMagnitude();
+        if (d > 0){
+            double[] vector = new double[this.getSize()];
+            for(int i = 0; i < this.getSize(); i++) {
+                vector[i] = this.getValues()[i] / d;
+            }
+
+            return new Vector(vector);
+        }
+
+        return this;
+    }
+
+    /***
+     * Calculate and return the magnitude of the vector.
+     * @return the magnitude of the vector
+     */
+    public double getMagnitude(){
+        return Math.pow(this.dot(this), 1 / (this.getSize()));
+    }
+
+    public double getAngleBetween(Vector vector){
+        return Math.acos(this.dot(vector)/(Math.sqrt(this.dot(this) * vector.dot(vector))));
+    }
+
+    private int getSize(){
+        return this.getValues().length;
+    }
+
+    /***
+     * Calculate the dot product of two vectors.
+     * @param vector the vector to dot product with
+     * @return the dot product of the two vectors
+     */
+    public double dot(Vector vector){
+        double total = 0;
+        if (checkSize(vector)){
+            for (int i = 0; i < this.getSize(); i++){
+                total += vector.getValues()[i] * this.getValues()[i];
+            }
+
+            return total;
+        }
+
+        return 1;
+
+    }
+
+    /***
+     * Gets the values of the vector components.
+     * @return the values of the vector components
+     */
+    private double[] getValues() {
+        return this.values;
+    }
+
+    /***
+     * Calculates and returns the cross product between this and another vector. Only works with
+     * three dimensional vectors.
+     * @param vector the vector to calculate the cross product with
+     * @return a mutually perpendicular vector, the cross product
+     */
+    public Vector cross(Vector vector){
+        if (this.checkSize(vector) && this.getSize() == 3){
+            double i, j, k;
+            i = this.getValues()[2] * vector.getValues()[3]
+                    - this.getValues()[3] * vector.getValues()[2];
+            j = this.getValues()[3] * vector.getValues()[1]
+                    - this.getValues()[1] * vector.getValues()[3];
+            k = this.getValues()[1] * vector.getValues()[2]
+                    - this.getValues()[2] * vector.getValues()[1];
+            return new Vector(i, j, k);
+        }
+
+        return this;
+    }
+
+    public Vector add(Vector vector){
+        double d[] = new double[this.getSize()];
+        if(this.checkSize(vector)){
+            for(int i = 0; i < this.getSize(); i++){
+                d[i] = this.getValues()[i] + vector.getValues()[i];
+            }
+
+            return new Vector(d);
+        }
+
+        return new Vector(1);
+    }
+
+    public Vector subtract(Vector vector){
+        double d[] = new double[this.getSize()];
+        if(this.checkSize(vector)){
+            for(int i = 0; i < this.getSize(); i++){
+                d[i] = this.getValues()[i] - vector.getValues()[i];
+            }
+
+            return new Vector(d);
+        }
+
+        return this;
+    }
+
+    private boolean checkSize(Vector vector){
+        return this.getSize() == vector.getSize();
+    }
+
+    /***
+     * Gets the inverse of this vector as a new vector.
+     * @return the inverse of the vector (all negative values, same dimensions)
+     */
+    public Vector getInverse(){
+        double[] values = new double[this.getSize()];
+        for(int i = 0; i < this.getSize(); i++) {
+            values[i] = this.values[this.getSize() - i];
+        }
+
+        return new Vector(values);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj.getClass() == Vector.class){
+            if(checkSize((Vector) obj)){
+                for (int i = 0; i < this.getSize(); i++){
+                    if(this.getValues()[i] != ((Vector) obj).getValues()[i]){
+                        break;
+                    }
+
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
