81fead400bf4e9c50e046a4579d9603741a80172
Devon <froggerman330+github@gmail.com>
2020-10-31 06:16:06 +1100

    made a shooting thread so that the action can complete independent of other contol (like driving or conveying) since we need an exact position.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Shooter.java
@@ -5,11 +5,50 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
+import java.util.concurrent.TimeUnit;
+
 /***
  * The class responsible for running and controlling the shooting mechanisms on the robot
  */
 public class Shooter extends Mechanism {
-    private DcMotor hammerArm;
+    private DcMotor hammerArmMotor;
+    private ShooterThread shootingThread;
+
+    private class ShooterThread extends Thread{
+        // As a note, I'm assuming Telemetry is not thread safe so not using it here
+        private DcMotor shooterMotor;
+        private double motorPower;
+
+        protected ShooterThread (DcMotor shooterMotor){
+            this.shooterMotor = shooterMotor;
+            init();
+        }
+
+        private void init(){
+            hammerArmMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            hammerArmMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+
+        protected boolean isRunning(){
+            return this.isAlive();
+        }
+
+        @Override
+        public void run() {
+            if (hammerArmMotor.getCurrentPosition() < 1440) {
+                hammerArmMotor.setTargetPosition(1440);
+                hammerArmMotor.setPower(motorPower);
+            }else {
+                hammerArmMotor.setPower(0);
+            }
+
+            feedMe();
+        }
+
+        public void setPower(double power) {
+            this.motorPower = power;
+        }
+    }
 
     /***
      * Shooter constructor
@@ -17,9 +56,8 @@ public class Shooter extends Mechanism {
      */
     public Shooter(HardwareMap map, Telemetry telemetry){
         super(telemetry);
-        hammerArm = map.get(DcMotor.class, "hammerMotor");
-        hammerArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        hammerArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        this.hammerArmMotor = map.get(DcMotor.class, MotorMap.SHOOTER_DC.getMotorName());
+        this.shootingThread = new ShooterThread(hammerArmMotor);
     }
 
     /***
@@ -27,26 +65,23 @@ public class Shooter extends Mechanism {
      * @param power the power with which to shoot a ring [0-1]
      */
     public void shoot(double power){
-        if (hammerArm.getCurrentPosition() < 1440) {
-            hammerArm.setTargetPosition(1440);
-            hammerArm.setPower(1);
-        }else {
-            hammerArm.setPower(0);
+        if(!shootingThread.isRunning()){
+            shootingThread.setPower(power);
+            shootingThread.run();
         }
-
     }
 
     /***
      * Shoots a ring with a preset power
      */
     public void shoot() {
-    shoot(1);
+        shoot(1);
     }
 
     /***
      * Loading the ring into the shooting mechanism
      */
-    public void feedMe(){
-
+    protected void feedMe(){
+        // TODO: enable the conveyor to be run again
     }
 }
