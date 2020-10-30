7e369a2412e1ce8a970972fb5991adfc3dfb5fe9
zsinclair1506 <zsinclair1506@gmail.com>
2020-10-30 12:03:31 +0930

    Added in Motor
    Attempted to create a working hammer arm for the shooting mechanism

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Shooter.java
@@ -1,19 +1,22 @@
 package org.firstinspires.ftc.teamcode.robot;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 /***
  * The class responsible for running and controlling the shooting mechanisms on the robot
  */
 public class Shooter extends Mechanism {
-    //private ?? motors
+    private DcMotor hammerArm;
 
     /***
      * Shooter constructor
      * @param map hardware map of the robot
      */
     public Shooter(HardwareMap map){
-
+        hammerArm = map.get(DcMotor.class, "hammerMotor");
+        hammerArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        hammerArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
     /***
@@ -21,6 +24,12 @@ public class Shooter extends Mechanism {
      * @param power the power with which to shoot a ring [0-1]
      */
     public void shoot(double power){
+        if (hammerArm.getCurrentPosition() < 1440) {
+            hammerArm.setTargetPosition(1440);
+            hammerArm.setPower(1);
+        }else {
+            hammerArm.setPower(0);
+        }
 
     }
 
