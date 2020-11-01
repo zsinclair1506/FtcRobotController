2651108bc3fddcdaa6f5c95ec854059ff5fac418
zsinclair1506 <zsinclair1506@gmail.com>
2020-11-01 15:21:03 +0930

    Updating Direction Methods
    Updated the directions methods class to begin using more up to date names and methods, renamed variable responsible for length of sleep from bedtime to sleeptime

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/lib/DirectionMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/lib/DirectionMethods.java
@@ -6,28 +6,32 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-
+/***
+ * @author Zac Sinclair
+ * @version 2.0.0
+ */
 
 public class DirectionMethods{
 
-public DcMotor left_drive;
-public DcMotor right_drive;
-public DcMotor rright_drive;
-public DcMotor rleft_drive;
-  /**
-   * Sleeps for the given amount of milliseconds, or until the thread is interrupted. This is
-   * simple shorthand for the operating-system-provided {@link Thread#sleep(long) sleep()} method.
-   *
-   * @param milliseconds amount of time to sleep, in milliseconds
-   * @see Thread#sleep(long)
-   */
+public DcMotor frontLeft;
+public DcMotor frontRight;
+public DcMotor backRight;
+public DcMotor backLeft;
+
      public DirectionMethods(HardwareMap hardwareMap) {
-          left_drive = hardwareMap.dcMotor.get("left_drive");
-          right_drive = hardwareMap.dcMotor.get("right_drive");
-          rright_drive = hardwareMap.dcMotor.get("rright_drive");
-          rleft_drive = hardwareMap.dcMotor.get("rleft_drive");
+          frontLeft = hardwareMap.dcMotor.get("frontLeft");
+          frontRight = hardwareMap.dcMotor.get("frontRight");
+          backRight = hardwareMap.dcMotor.get("backRight");
+          backLeft = hardwareMap.dcMotor.get("backLeft");
      }
-     
+
+    /***
+     * Sleeps for the given amount of milliseconds, or until the thread is interrupted. This is
+     * simple shorthand for the operating-system-provided {@link Thread#sleep(long) sleep()} method.
+     *
+     * @param milliseconds amount of time to sleep, in milliseconds
+     * @see Thread#sleep(long)
+     */
      public final void sleep(long milliseconds) {
      try   {
                Thread.sleep(milliseconds);
@@ -35,8 +39,12 @@ public DcMotor rleft_drive;
                Thread.currentThread().interrupt();
           }
      }
-     
-     public void forwardDrive(int bedtime) {
+
+    /***
+     *
+     * @param bedtime
+     */
+    public void forwardDrive(int bedtime) {
           left_drive.setPower(1);
           right_drive.setPower(-1);
           rright_drive.setPower(1);
@@ -99,58 +107,58 @@ public DcMotor rleft_drive;
           sleep(bedtime);
      }
      
-     public void backwardDriveSlow(int bedtime) {
+     public void backwardDriveSlow(int sleeptime) {
           left_drive.setPower(-0.3);
           right_drive.setPower(0.3);
           rright_drive.setPower(-0.3);
           rleft_drive.setPower(0.3);
-          sleep(bedtime);
+          sleep(sleeptime);
      }
      
-     public void leftDriveSlow(int bedtime) {
+     public void leftDriveSlow(int sleeptime) {
           left_drive.setPower(0.3);
           right_drive.setPower(-0.3);
           rright_drive.setPower(-0.3);
           rleft_drive.setPower(0.3);
-          sleep(bedtime);
+          sleep(sleeptime);
      }
      
-     public void rightDriveSlow(int bedtime) {
+     public void rightDriveSlow(int sleeptime) {
           left_drive.setPower(-0.3);
           right_drive.setPower(0.3);
           rright_drive.setPower(0.3);
           rleft_drive.setPower(-0.3);
-          sleep(bedtime);
+          sleep(sleeptime);
      }
      
-     public void donutsRightSlow(int bedtime) {
+     public void donutsRightSlow(int sleeptime) {
           left_drive.setPower(0.3);
           right_drive.setPower(0.3);
           rleft_drive.setPower(0.3);
           rright_drive.setPower(0.3);
-          sleep(bedtime);
+          sleep(sleeptime);
      }
      
-     public void donutsLeftSlow(int bedtime) {
+     public void donutsLeftSlow(int sleeptime) {
           left_drive.setPower(-0.3);
           right_drive.setPower(-0.3);
           rleft_drive.setPower(-0.3);
           rright_drive.setPower(-0.3);
-          sleep(bedtime);
+          sleep(sleeptime);
      }
      
-     public void leftDriveDless(int bedtime){
+     public void leftDriveDless(int sleeptime){
           left_drive.setPower(0.3);
           right_drive.setPower(-0.4);
           rleft_drive.setPower(-0.4);
           rright_drive.setPower(0.3);
-          sleep(bedtime);
+          sleep(sleeptime);
      }
-     public void rightDriveDless(int bedtime){
+     public void rightDriveDless(int sleeptime){
           left_drive.setPower(-0.3);
           right_drive.setPower(0.35);
           rleft_drive.setPower(-0.35);
           rright_drive.setPower(0.3);
-          sleep(bedtime);
+          sleep(sleeptime);
      }
 }
