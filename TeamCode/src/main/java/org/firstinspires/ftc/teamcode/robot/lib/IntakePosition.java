61effda5a977636c6e10b6422cc6881f2a412dce
Devon <froggerman330+github@gmail.com>
2020-11-02 12:28:39 +1100

    make position final (unchanging)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/lib/IntakePosition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/lib/IntakePosition.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.robot.lib;
 public enum IntakePosition {
     DROP_OFF(0.9), INTAKE(0.5);
 
-    private double position;
+    private final double position;
 
     IntakePosition(double position){
         this.position = position;
